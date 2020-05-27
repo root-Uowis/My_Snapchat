5191cc70cb9eeaf7ea2c1e89a0286cc7a7342505
simon <simoncanuel24@gmail.com>
2020-05-27 03:20:13 +0200

    day2

--- Snapchat/App.js
+++ Snapchat/App.js
@@ -1,19 +1,92 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { Container, Content } from 'native-base';
+import Swiper from 'react-native-swiper';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text> to start work!</Text>
-    </View>
+    <Container>
+      <Content>
+        <Swiper
+          loop={false}
+          showsPagination={false}
+          index={1}
+        >
+          <View style={styles.container}>
+            <Text style={styles.text}> Snap </Text>
+          </View >
+          <Swiper
+            loop={false}
+            showsPagination={false}
+            horizontal={false}
+            index={0}
+          >
+            <View style={{ flex: 1 }}>
+              <Camera style={{ flex: 1 }} type={type}>
+                <View
+                  style={{
+                    flex: 1,
+                    backgroundColor: 'transparent',
+                    flexDirection: 'row',
+                  }}>
+                  <TouchableOpacity
+                    style={{
+                      flex: 0.1,
+                      alignSelf: 'flex-end',
+                      alignItems: 'center',
+                    }}
+                    onPress={() => {
+                      setType(
+                        type === Camera.Constants.Type.back
+                          ? Camera.Constants.Type.front
+                          : Camera.Constants.Type.back
+                      );
+                    }}>
+                    <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                  </TouchableOpacity>
+                </View>
+              </Camera>
+            </View>
+            <View style={styles.container}>
+              <Text style={styles.text}> Memories </Text>
+            </View >
+          </Swiper>
+          <View style={styles.container}>
+            <Text style={styles.text} > Stories
+              </Text>
+          </View >
+        </Swiper>
+      </Content >
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF700'
   },
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
 });
